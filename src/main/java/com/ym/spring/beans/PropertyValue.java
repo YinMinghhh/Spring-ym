55965d433b001ef5f326508c949ae01a5d5b98a3
YinMing@loctek <yinming8086@gmail.com>
2025-05-07 09:49:38 +0800

    feat(PropertyValue): 补充bean属性值 1. 补充转换后的值

--- src/main/java/com/ym/spring/beans/PropertyValue.java
+++ src/main/java/com/ym/spring/beans/PropertyValue.java
@@ -1,14 +1,26 @@
 package com.ym.spring.beans;
 
 /**
+ * Bean属性值
  * @author yinMing
  * @since 2025/5/5 21:46
  */
 public class PropertyValue {
 
+    /**
+     * Bean名称
+     */
     private final String name;
 
+    /**
+     * 原始值
+     */
     private final Object value;
+
+    /**
+     * 转换后的值
+     */
+    private Object convertedValue;
 
     public PropertyValue(String name, Object value) {
         this.name = name;
@@ -21,5 +33,13 @@ public class PropertyValue {
 
     public Object getValue() {
         return value;
+    }
+
+    public Object getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(Object convertedValue) {
+        this.convertedValue = convertedValue;
     }
 }
